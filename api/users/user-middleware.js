c5f0fafa6cce5234a87f413043b19710c701b78f
Petr Gazarov <petrgazarov@gmail.com>
2021-07-26 20:59:08 -0700

    Add middleware methods

--- api/users/user-middleware.js
+++ api/users/user-middleware.js
@@ -1,1 +1,31 @@
-// user-middleware
+const User = require('./user-model');
+
+const validateUserBody = (req, res, next) => {
+  const userData = req.body;
+
+  if (!userData || !userData.username) {
+    res.status(400).json({ message: 'username is required' });
+  } else {
+    next();
+  }
+}
+
+const validateUserExists = (req, res, next) => {
+  const { id } = req.params;
+
+  User.findById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+
+        next();
+      } else {
+        res.status(404).json({ message: "Could not find user with given id." });
+      }
+    })
+}
+
+module.exports = {
+  validateUserBody,
+  validateUserExists,
+}
